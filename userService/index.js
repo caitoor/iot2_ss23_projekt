8d5ffe979d62dc797e9529ccd203958620f4e264
Fabian Rauch <mail@rauch-fabian.de>
2023-06-02 09:01:16 +0200

    230602_after course

--- userService/index.js
+++ userService/index.js
@@ -1,4 +1,33 @@
 require('dotenv').config();
+const bcrypt = require('bcryptjs');
+const pepper = process.env.PEPPER;
+
+try {
+    checkEnvVars();
+}
+catch (error) {
+    console.log(error);
+    process.exit(1);
+}
+
+function checkEnvVars() {
+    const requiredEnvVars = [
+        "DB_USER",
+        "DB_PASSWORD",
+        "DB_HOST",
+        "DB_DATABASE",
+        "API_PORT"
+    ];
+
+    const unsetEnvVars = requiredEnvVars.filter((envVar) => !(envVar in process.env));
+
+    if (unsetEnvVars.length > 0) {
+        console.error(`Fehler: Die folgenden Umgebungsvariablen sind nicht gesetzt: ${unsetEnvVars.join(', ')}`);
+        throw new Error("Umgebungsvariable nicht gesetzt.");
+    }
+}
+
+
 const express = require('express');
 const cors = require('cors');
 
@@ -52,21 +81,56 @@ app.get('/', (req, res) => {
 });
 
 app.post('/api/register', async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    console.log(username, password);
-    let user = {};
-    user.username = username;
-    user.password = password;
-    user.createdAt = new Date();
-    user.modifiedAt = new Date();
-    saveDocument(user);
-    res.status(200).send("Registration successful");
+    if (userCollection) {
+        const username = req.body.username;
+
+        const existingUser = await userCollection.findOne({ "username": username });
+        if (!existingUser) {
+
+            const password = req.body.password;
+            console.log(`Füge User ${username} zur Datenbank hinzu...`);
+            let user = {};
+            user.username = username;
+
+            let hashedPassword;
+
+            try {
+                const saltRounds = 10;
+                const salt = await bcrypt.genSalt(saltRounds);
+                hashedPassword = await bcrypt.hash(password + pepper, salt);
+                user.password = hashedPassword;
+            }
+            catch (error) {
+                console.log("password couldn't be hashed.");
+                res.status(500).send("password couldn't be hashed");
+            }
+            user.createdAt = new Date();
+            user.modifiedAt = new Date();
+            try {
+                await saveDocument(user);
+                res.status(200).send("Registration successful");
+            }
+            catch (error) {
+                console.log(error);
+                res.status(500).send("Error while saving userdata to database")
+            }
+        }
+        else {
+            console.log("username already taken.")
+            res.status(409).send("username already taken.");
+        }
+    }
+    else {
+        res.status(500).send("Error connecting to database.");
+    }
 });
 
-function saveDocument(msg) {
+async function saveDocument(msg) {
     msg.createdAt = new Date();
-    if (userCollection) {
-        userCollection.insertOne(msg);
+    try {
+        await userCollection.insertOne(msg);
+    }
+    catch (error) {
+        throw error;
     }
 }
